9f8c52a93e5f70738e029931127a0b9a252b65ab
Eduardo Santamaria <edsanav@gmail.com>
2022-01-13 22:26:53 +0100

    Little things

--- src/Scratch.hs
+++ src/Scratch.hs
@@ -3,6 +3,9 @@ module Scratch where
 bla :: Integer -> Integer -> Integer
 bla x y=  x + y
 
+getlength :: [Char] -> Integer
+getlength xs = sum [1 | _ <- xs]
+
 main :: IO ()
 main = do
   putStrLn "Scratching...."
