037992dea7a4fe17429e862b0e657271a0d2e74e
Eduardo Santamaria <edsanav@gmail.com>
2022-02-15 22:56:14 +0100

    Parsing

--- src/AOC2021.hs
+++ src/AOC2021.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 module AOC2021 where
 
 import Paths_haskell_playground
@@ -14,7 +16,7 @@ isLine :: Checkeable -> Bool
 isLine = all snd
 
 isWinner :: Panel -> Bool
-isWinner p = (any isLine p) -- || by cols
+isWinner p = any isLine p || any isLine (byCols p)
 
 getCol :: Int -> Panel -> Checkeable
 getCol i p = 
@@ -25,19 +27,22 @@ getCol i p =
 byCols :: Panel -> Panel
 byCols p = 
    let  withCol = [zip [0..] row | row <- p] :: [[(Int, (Int, Bool))]]
+        -- Here on applies == to the result of applying first to the two elements it's comparing
         grouped = groupBy ((==) `on` fst)  $ concat withCol
-   in [[snd x | x <-xs] | xs <- grouped]
-  
+   in [[snd x | x <-xs] | xs <- grouped]  
 
-readMyContent :: String -> ([Int], [String])
+readMyContent :: String -> ([Int], [Panel])
 readMyContent fileContent =
   let inputList = map T.unpack (T.splitOn (T.pack "\n\n") (T.pack fileContent))
-      numbers = map (read::String -> Int) $ words (head inputList)
-  in (numbers, tail inputList)
+      readNumsLine inp = map (read::String -> Int) (words inp)
+      numbers = readNumsLine (head inputList)
+--      readPanel pStr = map (\x -> (x, False)) $ map readNumsLine $ lines pStr
+      readPanel pStr = map (map (, False) . readNumsLine) (lines pStr)
+  in (numbers, map readPanel (tail inputList))
 
 run :: IO ()
 run = do
     file <- getDataFileName "resources/sample.txt"
     mydata <- readFile file
     let (instructions, panelString) = readMyContent mydata
-    putStrLn $ "Your data is: " ++ Data.List.intercalate "---" panelString
+    putStrLn $ "Your data is: " ++ Data.List.intercalate "---" (map show panelString)
