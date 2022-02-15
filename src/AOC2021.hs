bd66ecb0605bef911e5d660b8ea4d687bf420d08
Eduardo Santamaria <edsanav@gmail.com>
2022-02-15 20:08:17 +0100

    By columns

--- src/AOC2021.hs
+++ src/AOC2021.hs
@@ -2,7 +2,8 @@ module AOC2021 where
 
 import Paths_haskell_playground
 import qualified Data.Text as T
-import Data.List (intercalate)
+import Data.List (intercalate, groupBy)
+import Data.Function (on)
 
 
 type Checkeable = [(Int, Bool)]
@@ -12,11 +13,21 @@ type Panel = [Checkeable]
 isLine :: Checkeable -> Bool
 isLine = all snd
 
+isWinner :: Panel -> Bool
+isWinner p = (any isLine p) -- || by cols
+
 getCol :: Int -> Panel -> Checkeable
 getCol i p = 
   let withCol = [zip [0..] row | row <- p]
       cols = filter (\x -> fst x == i) (concat withCol)
   in map snd cols
+  
+byCols :: Panel -> Panel
+byCols p = 
+   let  withCol = [zip [0..] row | row <- p] :: [[(Int, (Int, Bool))]]
+        grouped = groupBy ((==) `on` fst)  $ concat withCol
+   in [[snd x | x <-xs] | xs <- grouped]
+  
 
 readMyContent :: String -> ([Int], [String])
 readMyContent fileContent =
