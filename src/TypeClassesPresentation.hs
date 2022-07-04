6eb59f5fe029c6f532b190f6d37d883f48b24b3e
Eduardo Santamaria <edsanav@gmail.com>
2022-07-04 23:11:08 +0200

    Type classes

---
+++ src/TypeClassesPresentation.hs
@@ -0,0 +1,11 @@
+module TypeClassesPresentation where
+
+-- :info Bool
+--Prelude> :info Bool
+--data Bool = False | True
+--instance Eq Bool
+--instance Ord Bool
+--instance Show Bool
+--instance Read Bool
+--instance Enum Bool
+--instance Bounded Bool
